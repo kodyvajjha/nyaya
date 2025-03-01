f341928ffbb25a4eb2a9386e27d856ee825d49ee
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2025-03-01 12:03:03 -0800

    name: basic scaffolding for name type

---
+++ lib/name.ml
@@ -0,0 +1,7 @@
+(* This module contains the first of the Lean kernel's primitive types; the
+   type of names. It provides kernel items with a way of addressing things. *)
+
+type t =
+  | Anon
+  | Str of (t * string)
+  | Num of (t * int)
