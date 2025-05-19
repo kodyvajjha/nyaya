c6a90f3a51ced78fdeeb1582391b688606e4e43e
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2025-05-18 22:14:37 -0700

    expr: wip resolver

--- lib/expr.ml
+++ lib/expr.ml
@@ -1,5 +1,5 @@
-(* TODO: Expressions need to store some data inline or cache it somewhere to prevent prohibitively expensive recomputation. 
-Perhaps we could do hashconsing.*)
+(* TODO: Expressions need to store some data inline or cache it somewhere to prevent prohibitively expensive recomputation.
+   Perhaps we could do hashconsing.*)
 
 type binfo =
   | Default
@@ -7,6 +7,13 @@ type binfo =
   | InstanceImplicit
   | StrictImplicit
 [@@deriving show]
+
+let binfo_of_ast (info : Nyaya_parser.Ast.info) =
+  match info with
+  | Nyaya_parser.Ast.IBD -> Default
+  | Nyaya_parser.Ast.IBI -> Implicit
+  | Nyaya_parser.Ast.IBS -> StrictImplicit
+  | Nyaya_parser.Ast.IBC -> InstanceImplicit
 
 type literal =
   | NatLit of Z.t [@printer Z.pp_print]
@@ -27,18 +34,12 @@ type t =
     }
   | Sort of Level.t
   | App of t * t
-
-(**   binderName      body
-      |            |
-fun (foo : Bar) => 0 
-            |         
-        binderType     *)
   | Lam of {
       name: Name.t;
       btype: t;
-      (* binderInfo is reflected by the style of brackets used 
-      to surround the binder*)
-      binfo: binfo; 
+      (* binderInfo is reflected by the style of brackets used
+         to surround the binder*)
+      binfo: binfo;
       body: t;
     }
   | Forall of {
@@ -68,3 +69,59 @@ let (foo : Bar) := 0; foo
     }
   | Literal of literal
 [@@deriving show]
+
+open Nyaya_parser
+
+let table (ast : Ast.t) : (Ast.eidx, t) Hashtbl.t =
+  let resolved_table = Hashtbl.create (CCList.length ast.items) in
+  let item_table = Ast.Hashed.items ast in
+  let rec resolve (eid : int) =
+    match Hashtbl.find_opt resolved_table eid with
+    | Some expr -> expr
+    | None ->
+      let resolved =
+        let open CCOption in
+        let* item = Hashtbl.find_opt item_table eid in
+        let+ expr = Ast.Item.get_expr item in
+        match expr with
+        | Ast.Expr.EVExpr { num; _ } -> BoundVar num
+        | Ast.Expr.ESExpr { uid; _ } ->
+          Sort (Hashtbl.find (Level.table ast) uid)
+        | Ast.Expr.ECExpr { nid; uids; _ } ->
+          let name = Hashtbl.find (Name.table ast) nid in
+          let uparams =
+            CCList.(uids >|= fun id -> Hashtbl.find (Level.table ast) id)
+          in
+          Const { name; uparams }
+        | Ast.Expr.EAExpr { eid2; eid3; _ } -> App (resolve eid2, resolve eid3)
+        | Ast.Expr.ELExpr { info; nid; eid2; eid3; _ } ->
+          let name = Hashtbl.find (Name.table ast) nid in
+          let binfo = binfo_of_ast info in
+          let btype = resolve eid2 in
+          let body = resolve eid3 in
+          Lam { name; binfo; btype; body }
+        | Ast.Expr.EPExpr { info; nid; eid2; eid3; _ } ->
+          let name = Hashtbl.find (Name.table ast) nid in
+          let binfo = binfo_of_ast info in
+          let btype = resolve eid2 in
+          let body = resolve eid3 in
+          Forall { name; binfo; btype; body }
+        | Ast.Expr.EZExpr _ -> assert false
+        | Ast.Expr.EJExpr _ -> assert false (* NYI *)
+        | Ast.Expr.ELNExpr _ -> assert false (* NYI *)
+        | Ast.Expr.ELSExpr _ -> assert false (* NYI *)
+        | Ast.Expr.EMExpr _ -> assert false (* NYI *)
+      in
+
+      (match resolved with
+      | Some nm ->
+        Hashtbl.add resolved_table eid nm;
+        nm
+      | None ->
+        failwith
+        @@ CCFormat.sprintf "@[expr resolution failed for id %d@.@]" eid)
+  in
+
+  (* Resolve every name *)
+  Hashtbl.iter (fun eid _ -> ignore (resolve eid)) item_table;
+  resolved_table
