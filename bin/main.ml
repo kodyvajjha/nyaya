b8561b5ec12d8f3f2c43e1129641309da603806a
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2025-07-26 17:35:28 -0700

    tc: well-posedness checks of the decls in the environment

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,39 @@
+(* let () =
+   let open Nyaya_parser in
+   Logs.set_reporter (Lexer.Logger.reporter Format.std_formatter);
+   Logs.set_level (Some Logs.Info);
+
+   let result = Main.parse_from_file "test/parser/init.export" in
+
+   let env = Nyaya.Env.mk result in
+   CCFormat.printf "%a" Nyaya.Env.pp env *)
+
 let () =
   let open Nyaya_parser in
   Logs.set_reporter (Lexer.Logger.reporter Format.std_formatter);
-  Logs.set_level (Some Logs.Info);
-
-  let result = Main.parse_from_file "test/parser/init.export" in
+  Logs.set_level (Some Logs.Error);
+  let result = Main.parse_from_file "test/parser/universes.export" in
   let env = Nyaya.Env.mk result in
-  CCFormat.printf "%a" Nyaya.Env.pp env
+  (* CCFormat.printf "@[Random el: %a@." Nyaya.Expr.pp
+     (Util.get_random_el expr_tbl); *)
+  CCFormat.printf "%a@.@]"
+    (CCHashtbl.pp ~pp_sep:CCFormat.(newline) Nyaya.Name.pp Nyaya.Decl.pp)
+    env;
+
+  (* Check well-posedness of all declarations *)
+  let all_well_posed = Nyaya.Tc.check_all_well_posed env in
+  CCFormat.printf "All declarations well-posed: %b@." all_well_posed;
+
+  Nyaya.Tc.typecheck env
+
+(*
+   let () =
+     let open Nyaya.Level in
+     let open Nyaya.Name in
+     let u = Str (Anon, "u") in
+     let v = Str (Anon, "v") in
+     let l1 = Max (Param u, IMax (Param u, Succ (Param v))) in
+     let _l2 = subst l1 (Str (Anon, "u")) Zero in
+     let l3 = Max (Param u, IMax (Succ Zero, Zero)) in
+     CCFormat.printf "Before : %a @. After : %a" Nyaya.Level.pp l1 Nyaya.Level.let open CCRandom inpp
+       (simplify l3) *)
--- lib/tc.ml
+++ lib/tc.ml
@@ -4,17 +4,49 @@ module Logger = Nyaya_parser.Util.MakeLogger (struct
   let header = "Checker"
 end)
 
-let well_posed (info : Decl.decl_info) =
-  let rec dup_exist = function
-    | [] -> false
-    | hd :: tl -> List.exists (( = ) hd) tl || dup_exist tl
-  in
-  dup_exist info.uparams |> not && Expr.has_free_vars info.ty |> not
-
 let infer (_env : Env.t) (expr : Expr.t) : Expr.t =
   match (expr : Expr.t) with
   | Expr.Sort u -> Expr.Sort (Level.Succ u)
   | _ -> Logger.err "failed inferring: %a" (TypeError expr) Expr.pp expr
+
+(* Helper to extract decl_info from any declaration *)
+let get_decl_info (decl : Decl.t) : Decl.decl_info =
+  match decl with
+  | Decl.Axiom info -> info
+  | Decl.Def { info; _ } -> info
+  | Decl.Thm { info; _ } -> info
+  | Decl.Opaque { info; _ } -> info
+  | Decl.Quot { info } -> info
+  | Decl.Inductive { info; _ } -> info
+  | Decl.Ctor { info; _ } -> info
+  | Decl.Rec { info; _ } -> info
+
+let well_posed (env : Env.t) (info : Decl.decl_info) : bool =
+  let rec dup_exist = function
+    | [] -> false
+    | hd :: tl -> List.exists (( = ) hd) tl || dup_exist tl
+  in
+  let no_dup_uparams = dup_exist info.uparams |> not in
+  let no_free_vars = Expr.has_free_vars info.ty |> not in
+  let type_is_sort =
+    try
+      match infer env info.ty with
+      | Expr.Sort _ -> true
+      | _ -> false
+    with TypeError _ -> false
+  in
+  no_dup_uparams && no_free_vars && type_is_sort
+
+(* Check well-posedness of all declarations in the environment *)
+let check_all_well_posed (env : Env.t) : bool =
+  Hashtbl.fold
+    (fun name decl acc ->
+      let info = get_decl_info decl in
+      let is_well_posed = well_posed env info in
+      if not is_well_posed then
+        Logger.info "Declaration %a is not well-posed" Name.pp name;
+      acc && is_well_posed)
+    env true
 
 let whnf (expr : Expr.t) : Expr.t =
   match expr with
