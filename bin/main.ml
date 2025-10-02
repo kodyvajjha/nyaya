72cc0a2a142275f9f90d72ec18ce1a50f6c50f21
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2025-10-02 09:57:34 +0530

    nyaya: main binary checks init.lean

--- bin/main.ml
+++ bin/main.ml
@@ -1,15 +1,9 @@
 let () =
   let open Nyaya_parser in
   Logs.set_reporter (Lexer.Logger.reporter Format.std_formatter);
-  Logs.set_level (Some Logs.Debug);
-  let result = Main.parse_from_file "test/parser/id.export" in
+  Logs.set_level (Some Logs.Info);
+  let result = Main.parse_from_file "test/parser/init.export" in
   let env = Nyaya.Env.mk result in
-  (* CCFormat.printf "@[Random el: %a@." Nyaya.Expr.pp
-     (Util.get_random_el expr_tbl); *)
-  CCFormat.printf "%a@.@]"
-    (CCHashtbl.pp ~pp_sep:CCFormat.(newline) Nyaya.Name.pp Nyaya.Decl.pp)
-    env;
-
   (* Check well-posedness of all declarations *)
   let all_well_posed = Nyaya.Tc.check_all_well_posed env in
   CCFormat.printf "All declarations well-posed: %b@." all_well_posed;
