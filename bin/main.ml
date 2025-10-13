f27b54528c486941f5884b38e5415e3850344ba7
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2025-10-13 10:31:34 +0530

    nyaya: modify bin to print output to file

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,11 @@
 let () =
+  let _file_outchannel =
+    Format.formatter_of_out_channel (open_out "output.txt")
+  in
   let open Nyaya_parser in
   Logs.set_reporter (Lexer.Logger.reporter Format.std_formatter);
-  Logs.set_level (Some Logs.Info);
-  let result = Main.parse_from_file "test/parser/init.export" in
+  Logs.set_level (Some Logs.Debug);
+  let result = Main.parse_from_file "test/parser/outparam_mock.export" in
   let env = Nyaya.Env.mk result in
   (* Check well-posedness of all declarations *)
   let all_well_posed = Nyaya.Tc.check_all_well_posed env in
