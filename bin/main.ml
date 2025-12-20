47a007154e840e8aa6ddfe57e41edb720d1ec5a3
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2025-12-20 10:20:00 -0800

    nyaya: make changes to main exe

--- bin/main.ml
+++ bin/main.ml
@@ -3,10 +3,11 @@ let () =
        Format.formatter_of_out_channel (open_out "output.txt")
      in *)
   let open Nyaya_parser in
+  Printexc.record_backtrace true;
   Logs.set_reporter (Lexer.Logger.reporter Format.std_formatter);
-  Logs.set_level (Some Logs.Debug);
+  Logs.set_level (Some Logs.Info);
   (* Construct the AST from export file. *)
-  let result = Main.parse_from_file "test/parser/structure.export" in
+  let result = Main.parse_from_file "test/parser/init.export" in
   (* Construct the environment from the AST. *)
   let env = Nyaya.Env.mk result in
   (* Typecheck the environment. *)
