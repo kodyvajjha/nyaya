91702a9f33181f8e63e79929655cdeb43ced2545
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2026-07-11 00:28:09 -0700

    Modify main executable to take in new ndjson export format tests

--- bin/main.ml
+++ bin/main.ml
@@ -2,8 +2,14 @@ let () =
   let open Nyaya_parser in
   Logs.set_reporter (Lexer.Logger.reporter Format.err_formatter);
   Logs.set_level (Some Logs.Info);
-  (* Construct the AST from export file. *)
-  let result = Main.parse_from_file "test/parser/init.export" in
+  (* Path to a new-format (ndjson) export file: taken from the command line if
+     given, otherwise a default sample under test/good. *)
+  let file =
+    if Array.length Sys.argv > 1 then Sys.argv.(1)
+    else "test/good/tutorial/001_basicDef.ndjson"
+  in
+  (* Construct the AST from the ndjson export file. *)
+  let result = Ndjson.parse_from_file file in
   (* Construct the environment from the AST. *)
   let env = Nyaya.Env.mk result in
   (* Typecheck the environment. *)
